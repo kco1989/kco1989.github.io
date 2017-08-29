858aa6b0c0ee4a1a0d64142e1465ae29474f46a4
lvsw <lsw@eeepay.cn>
2017-08-29 14:10:54 +0800

    用直线画爱心

---
+++ js/love2.js
@@ -0,0 +1,120 @@
+/**
+ * Created by 666666 on 2017/8/28.
+ */
+$(function () {
+    var content = {
+        N : 80,
+        circle:{
+            radius: 100,
+            x: 200,
+            y: 200
+        },
+        points: []
+    };
+    var stage = new Kinetic.Stage({
+       container: 'container',
+       width: 600,
+       height: 400
+    });
+    var layer = new Kinetic.Layer();
+    for (var i = 0; i < 2 * content.N; i ++){
+       var jiao = Math.PI / content.N * i;
+        content.points.push({
+           x: Math.cos(jiao - Math.PI / 2) * content.circle.radius + content.circle.x,
+           y: Math.sin(jiao - Math.PI / 2) * content.circle.radius + content.circle.y
+       })
+    }
+    console.log("%o", content.points);
+    var circle = new Kinetic.Circle({
+       radius: content.circle.radius,
+       x: content.circle.x,
+       y: content.circle.y,
+       fill: 'yellow',
+       stroke: 'black',
+       strokeWidth: 4
+    });
+    layer.add(circle);
+    function get(array, index) {
+        return array[(index + array.length) % array.length];
+    }
+    var lines = [];
+    for (var i = 0; i <= content.N / 2; i ++) {
+        lines.push((function (index) {
+            return new Kinetic.Line({
+                points: [
+                    get(content.points, index).x,
+                    get(content.points, index).y,
+                    get(content.points, index + content.N / 2).x,
+                    get(content.points, index + content.N / 2).y
+                ],
+                stroke: 'red',
+                strokeWidth: 1
+            });
+        })(i));
+    }
+    for (var i = 0; i <= content.N / 2; i ++) {
+        lines.push((function (index) {
+            return new Kinetic.Line({
+                points: [
+                    get(content.points, index + content.N).x,
+                    get(content.points, index + content.N).y,
+                    get(content.points, index + content.N * 3 / 2).x,
+                    get(content.points, index + content.N * 3 / 2).y
+                ],
+                strokeWidth: 1,
+                stroke: 'red'
+            });
+        })(i));
+    }
+    for (var i = 0; i <= content.N / 2; i ++) {
+        lines.push((function (index) {
+            return new Kinetic.Line({
+                points: [
+                    get(content.points, content.N / 2 - index).x,
+                    get(content.points, content.N / 2 - index).y,
+                    get(content.points, content.N * 2 - 2 * index).x,
+                    get(content.points, content.N * 2 - 2 * index).y
+                ],
+                strokeWidth: 1,
+                stroke: 'red'
+            });
+        })(i));
+    }
+    for (var i = 0; i <= content.N / 2; i ++){
+        lines.push((function (index) {
+            return new Kinetic.Line({
+                points:[
+                    get(content.points, content.N * 2 - index).x,
+                    get(content.points, content.N * 2 - index).y,
+                    get(content.points, content.N - 2 * index).x,
+                    get(content.points, content.N - 2 * index).y
+                ],
+                strokeWidth: 1,
+                stroke: 'red'
+            });
+        })(i));
+    }
+
+    stage.add(layer);
+    var index = 0;
+    // debugger;
+    // stage.onEnterFrame(function (frame) {
+    //     debugger;
+    //     if (index < lines.length){
+    //         layer.add(lines[index ++]);
+    //         layer.draw();
+    //     }else{
+    //         stage.stop();
+    //     }
+    // });
+    var anim = new Kinetic.Animation(function(frame) {
+            // debugger;
+            if (index < lines.length){
+                layer.add(lines[index ++]);
+                layer.draw();
+            }else{
+                stage.stop();
+            }
+     }, layer);
+    anim.start();
+});
